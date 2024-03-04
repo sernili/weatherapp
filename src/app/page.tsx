ebce7b6560ea2ed3485265ba1870469432863348
sernili <li.sachse@gmail.com>
2024-03-04 15:19:04 +0100

    add todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import WeeklyView from "@/components/weekly-view/WeeklyView";
 
+// TODO: add different views for different cities
 export default function Home() {
   return (
     <main className="bg-gradient-radial from-gray-800 to-black h-screen w-screen flex justify-center">
