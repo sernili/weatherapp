ed765a2f5f0e87753050c7315e04e85fb71e51bf
Lisa Sachse <li.sachse@gmail.com>
2025-09-11 16:02:30 +0200

    draft: improved logic

--- src/components/WeeklyView.tsx
+++ src/components/WeeklyView.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import WeeklyViewItem from "./WeeklyViewItem";
 import { WeatherDay, WeatherTimeline } from "@/types/weather";
 import { WateringRequirements, WateringLast } from "@/types/watering";
-import next from "next";
 
 export default function WeeklyView({
   weatherTimeline,
@@ -15,38 +14,26 @@ export default function WeeklyView({
   const waterRequirements: WateringRequirements = 3;
   const lastWatering: WateringLast = new Date("2025-09-07");
 
-  // Watering Logic
-  const today: Date = new Date();
-  const daysSinceLastWatering = getDayDifference(lastWatering, today);
-  const weatherArray = [
-    ...weatherTimeline.past,
-    weatherTimeline.today,
-    ...weatherTimeline.future,
-  ];
-  const indexToday = daysRange;
-  const indexLastWatering =
-    indexToday - daysSinceLastWatering < 0
-      ? 0
-      : indexToday - daysSinceLastWatering;
-
-  const nextWater_Temp = getNextWatering_Temperature(
-    indexLastWatering,
-    weatherArray,
-    waterRequirements
-  );
-
-  const additionalDays_Rain = getAdditionalDays_Rain(
-    indexLastWatering,
-    weatherArray,
-    waterRequirements,
-    nextWater_Temp
-  );
-
-  console.log("Next: ", nextWater_Temp);
+  const [weatherArray, setWeatherArray] = useState<WeatherDay[]>([]);
 
   useEffect(() => {
-    console.log("DATA", weatherTimeline);
+    setWeatherArray([
+      ...weatherTimeline.past,
+      weatherTimeline.today,
+      ...weatherTimeline.future,
+    ]);
   }, [weatherTimeline]);
+
+  // useEffect(() => {
+  //   setDaysUntilWatering(
+  //     getNextWatering_Temperature(
+  //       lastWatering,
+  //       lastRain,
+  //       waterRequirements,
+  //       weatherArray
+  //     )
+  //   );
+  // }, [lastRain]);
 
   return (
     <div className="grid ">
@@ -55,12 +42,9 @@ export default function WeeklyView({
           <WeeklyViewItem
             key={data.date}
             weather={data}
-            daysSinceLastWatering={getDayDifference(
-              lastWatering,
-              new Date(data.date)
-            )}
-            nextWater_Temp={nextWater_Temp}
-            additionalDays_Rain={additionalDays_Rain}
+            lastWater={lastWatering}
+            waterRequirements={waterRequirements}
+            weatherArray={weatherArray}
           />
         ))}
       </div>
@@ -68,67 +52,42 @@ export default function WeeklyView({
   );
 }
 
-function getDayDifference(date1: Date, date2: Date) {
-  const oneDay = 1000 * 60 * 60 * 24; // ms in a day
-  const diffInTime = date2.getTime() - date1.getTime();
-  return Math.round(diffInTime / oneDay);
-}
+// function getNextWatering_Temperature(
+//   lastWatering: Date,
+//   lastRain: Date,
+//   waterRequirements: WateringRequirements,
+//   weatherArray: WeatherDay[]
+// ) {
 
-function getNextWatering_Temperature(
-  indexLastWatering: number,
-  weatherArray: WeatherDay[],
-  waterRequirements: WateringRequirements
-) {
-  let daysUntilWatering = 0;
-  const tempNextDay = weatherArray[indexLastWatering + 1].day.avgtemp_c;
+//   let daysUntilWatering = 0;
 
-  const tempRules = {
-    cold: {
-      1: 5,
-      2: 4,
-      3: 3,
-    },
-    medium: {
-      1: 3,
-      2: 2,
-      3: 2,
-    },
-    hot: {
-      1: 2,
-      2: 1,
-      3: 1,
-    },
-  };
+//   const tempNextDay = weatherArray[indexLastWatering + 1].day.avgtemp_c;
 
-  if (tempNextDay < 20) {
-    daysUntilWatering = tempRules["cold"][waterRequirements];
-  } else if (tempNextDay >= 20 && tempNextDay < 30) {
-    daysUntilWatering = tempRules["medium"][waterRequirements];
-  } else {
-    daysUntilWatering = tempRules["hot"][waterRequirements];
-  }
+//   const tempRules = {
+//     cold: {
+//       1: 5,
+//       2: 4,
+//       3: 3,
+//     },
+//     medium: {
+//       1: 3,
+//       2: 2,
+//       3: 2,
+//     },
+//     hot: {
+//       1: 2,
+//       2: 1,
+//       3: 1,
+//     },
+//   };
 
-  return daysUntilWatering;
-}
+//   if (tempNextDay < 20) {
+//     daysUntilWatering = tempRules["cold"][waterRequirements];
+//   } else if (tempNextDay >= 20 && tempNextDay < 30) {
+//     daysUntilWatering = tempRules["medium"][waterRequirements];
+//   } else {
+//     daysUntilWatering = tempRules["hot"][waterRequirements];
+//   }
 
-function getAdditionalDays_Rain(
-  indexLastWatering: number,
-  weatherArray: WeatherDay[],
-  waterRequirements: WateringRequirements,
-  nextWater_Temp: number
-) {
-  let additionalDays = 0;
-  const weatherSinceWater = weatherArray.slice(indexLastWatering);
-
-  weatherSinceWater.forEach((day, index) => {
-    if (
-      index <= nextWater_Temp + additionalDays &&
-      day.day.totalprecip_mm >= 5
-    ) {
-      additionalDays++;
-    }
-  });
-
-  console.log("additionalDays: ", additionalDays);
-  return additionalDays;
-}
+//   return daysUntilWatering;
+// }
--- src/components/WeeklyViewItem.tsx
+++ src/components/WeeklyViewItem.tsx
@@ -1,43 +1,95 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
 import { WeatherDay } from "@/types/weather";
 import { Divide } from "lucide-react";
 
+const rainThreshold = 2;
+
 export default function WeeklyViewItem({
   weather,
-  daysSinceLastWatering,
-  nextWater_Temp,
-  additionalDays_Rain,
+  lastWater,
+  waterRequirements,
+  weatherArray,
 }: {
   weather: WeatherDay;
-  daysSinceLastWatering: number;
-  nextWater_Temp: number;
-  additionalDays_Rain: number;
+  lastWater: Date;
+  waterRequirements: number;
+  weatherArray: WeatherDay[];
 }) {
-  const dayDate = new Date(weather.date);
-  const dayName = dayDate.toLocaleDateString("en-US", { weekday: "long" });
+  const todayDate = new Date();
+  const currDate = new Date(weather.date);
+  const currDateName = todayDate.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+
+  const isToday = todayDate.toDateString() === currDate.toDateString();
+
+  const daysSinceLastWatering = getDayDifference(lastWater, currDate);
+  const isLastWater = daysSinceLastWatering === 0;
+
+  const isRainDay = weather.day.totalprecip_mm >= rainThreshold;
+  const lastRain: Date | undefined = isRainDay
+    ? currDate
+    : getLastRain(weatherArray, currDate);
+  const daysSinceLastRain =
+    lastRain === undefined
+      ? daysSinceLastWatering
+      : getDayDifference(lastRain, currDate);
+
+  const daysSinceLastWaterOrRain = Math.min(
+    daysSinceLastWatering,
+    daysSinceLastRain
+  );
+
+  const tempRules: Record<"cold" | "medium" | "hot", Record<number, number>> = {
+    cold: {
+      1: 5,
+      2: 4,
+      3: 3,
+    },
+    medium: {
+      1: 3,
+      2: 2,
+      3: 2,
+    },
+    hot: {
+      1: 2,
+      2: 1,
+      3: 1,
+    },
+  };
+
+  const currAvgTemp = weather.day.avgtemp_c;
+  const currTempRule =
+    currAvgTemp < 20
+      ? "cold"
+      : currAvgTemp >= 20 && currAvgTemp < 30
+      ? "medium"
+      : "hot";
+  const currDaysUntilWaterNeeded: number =
+    tempRules[currTempRule][waterRequirements];
+
+  const isTempDay =
+    !isRainDay &&
+    !isLastWater &&
+    daysSinceLastWaterOrRain < currDaysUntilWaterNeeded;
+
+  const isWaterDay = currDaysUntilWaterNeeded === daysSinceLastWaterOrRain;
+
+  // Styling
   const cssContainerBase =
     "text-white p-4 rounded-xl text-center mb-8 space-y-4 shadow-lg w-32 h-full flex flex-col justify-between items-center  gap-4";
   const cssNotToday = "bg-secondary";
   const cssToday = "scale-110 bg-accent";
 
-  const isToday = dayDate.toDateString() === new Date().toDateString();
-  const isLastWatering = daysSinceLastWatering === 0;
-
   const cssContainer = isToday
     ? `${cssContainerBase} ${cssToday}`
     : `${cssContainerBase} ${cssNotToday}`;
-
-  const isPlusDay = weather.day.totalprecip_mm >= 5;
-  const isWaterDay =
-    daysSinceLastWatering == nextWater_Temp + additionalDays_Rain;
-  const isTempDay =
-    !isPlusDay && daysSinceLastWatering < nextWater_Temp + additionalDays_Rain;
 
   return (
     <div className="flex flex-col">
       <div className={cssContainer}>
         <div className="">
-          <h2 className="font-bold">{dayName}</h2>
+          <h2 className="font-bold">{currDateName}</h2>
           <p className=" mb-4">{weather.date}</p>
           <div>
             <p className="text-3xl">{weather.day.avgtemp_c}°C</p>
@@ -51,17 +103,98 @@ export default function WeeklyViewItem({
         <p>{weather.day.totalprecip_mm}mm</p>
       </div>
 
-      {isLastWatering && <div>💦✅</div>}
+      {isLastWater && <div>💦✅</div>}
 
       {daysSinceLastWatering > 0 && (
         <>
           <div>{daysSinceLastWatering}</div>
 
-          {isPlusDay && <div>+</div>}
+          {isRainDay && <div>🌧️</div>}
           {isTempDay && <div>TEMP</div>}
           {isWaterDay && <div>WATER</div>}
         </>
       )}
     </div>
   );
+}
+
+function getDayDifference(date1: Date, date2: Date) {
+  const oneDay = 1000 * 60 * 60 * 24; // ms in a day
+  const diffInTime = date2.getTime() - date1.getTime();
+  return Math.round(diffInTime / oneDay);
+}
+
+// function getNextWater(
+//   indexLastWatering: number,
+//   weatherArray: WeatherDay[],
+//   waterRequirements: WateringRequirements
+// ) {
+//   const weatherSinceWater = weatherArray.slice(indexLastWatering);
+//   const nextWater_Temp = getNextWatering_Temperature(
+//     indexLastWatering,
+//     weatherSinceWater,
+//     waterRequirements
+//   );
+
+//   let indicesRain: number[] = [];
+
+//   weatherSinceWater.forEach((day, index) => {
+//     if (day.day.totalprecip_mm > 5) {
+//       indicesRain.push(index);
+//     }
+//   });
+
+//   console.log("INDEX RAIN ", indicesRain);
+
+//   const additionalDays_Rain = getAdditionalDays_Rain(
+//     indexLastWatering,
+//     weatherArray,
+//     waterRequirements,
+//     nextWater_Temp
+//   );
+// }
+
+// function getAdditionalDays_Rain(
+//   indexLastWatering: number,
+//   weatherArray: WeatherDay[],
+//   waterRequirements: WateringRequirements,
+//   nextWater_Temp: number
+// ) {
+//   let additionalDays = 0;
+//   const weatherSinceWater = weatherArray.slice(indexLastWatering);
+
+//   weatherSinceWater.forEach((day, index) => {
+//     if (
+//       index <= nextWater_Temp + additionalDays &&
+//       day.day.totalprecip_mm >= 5
+//     ) {
+//       additionalDays++;
+//     }
+//   });
+
+//   console.log("additionalDays: ", additionalDays);
+//   return additionalDays;
+// }
+
+function getLastRain(weatherArray: WeatherDay[], currDate: Date) {
+  const currIndex = weatherArray.findIndex(
+    (day) => new Date(day.date).toDateString() === currDate.toDateString()
+  );
+
+  let lastRain: Date | undefined = undefined;
+  let index = currIndex;
+
+  while (lastRain === undefined && index > 0) {
+    index--;
+
+    console.log("INDEX: ", index);
+
+    // TODO: put in varibale
+    if (weatherArray[index].day.totalprecip_mm > rainThreshold) {
+      lastRain = new Date(weatherArray[index].date);
+    }
+  }
+
+  console.log(lastRain);
+  return lastRain;
 }
